744d6a771ce719a850322b198ebc8fd8e4ee15d0
Ariel <ariel.7198@hotmail.com>
2019-07-20 14:21:16 -0300

    Correções no cadtro de comboio

--- www/src/registerConvoy.php
+++ www/src/registerConvoy.php
@@ -1,9 +1,9 @@
 <?php
-    require_once (seguranca.php)
+    require_once ('seguranca.php');
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         echo "recebeu o form";
-        $date = ($_POST['date'])
+        $date = ($_POST['date']);
         echo "<br>data recebida: ", $date;
         $pretime = ($_POST['pretime']);
         echo "<br> horario de concentracao: ", $pretime;
@@ -19,9 +19,11 @@
         echo "<br> px: ", $px;
         $server = ($_POST['server']);
         echo "<br> server: ", $server;
+        $instructions = ($_POST['instructions']);
+        echo "<br> instructions: ", $instructions;
 
-        if (registerConvoy($user, $password) == true){
-            header ("Location: ../templates/colaborador.php");
+        if (registerConvoy($pretime, $time, $originCity, $destinationCity, $date, $saveLink, $px, $server, $instructions) == true){
+//            header ("Location: ../templates/colaborador.php");
             echo "deu certo";
             echo "sessao vale: ", $_SESSION['usuarioNome'];
         } else {
