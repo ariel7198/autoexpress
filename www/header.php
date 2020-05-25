5a8c7c5a9bc291fcbb50f5c2ed0b0c871df55a1c
Ariel <ariel.7198@hotmail.com>
2020-05-25 15:58:26 -0300

    criado rodapÃ© unico replicado para todas as paginas

---
+++ www/header.php
@@ -0,0 +1,69 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"> <img src="https://static.tumblr.com/jjqbi6t/YWnpe5fxy/logo.png"
+                        class="aex-logo img-circle"> </a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li> <a href="index.php"> INÍCIO </a></li>
+                    <li> <a href="index.php#sobre"> SOBRE </a></li>
+                    <!--<li> <a href="https://autoexpresslogistica.tumblr.com/colaboradores"> COLABORADORES </a></li> -->
+                    <li> <a href="index.php#contato"> CONTATO </a></li>
+                    <li> <a href="recrutamento.html"> RECRUTAMENTO </a></li>
+                    <li> <a href="colaboradores.html"> EQUIPE </a></li>
+                    <li> <?php 
+                            if (!isset($_SESSION['usuarioID'])) { 
+                                echo "<button type='button' class='btn btn-info btn-login'  data-toggle='modal'
+                                                        data-target='#modalLogin'>LOGIN</button>'}";
+                                
+                            } 
+                                else { 
+                                    echo "<a href='colaborador.php'> COLABORADOR </a> ";
+                                   }
+                        ?>                
+                    <li>
+                    
+
+                </ul>
+            </div>
+            <div class="modal fade " tabindex="-1" role="dialog" id="modalLogin">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header header-promocao">
+                            <h5 class="modal-title-center">LOGIN</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="login.php" id="loginForm" method="POST" autocomplete="on">
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">USUÁRIO</label>
+                                    <input type="text" class="form-control" id="user" name="user" aria-describedby="emailHelp" placeholder="Usuário" required>
+                                    
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">SENHA</label>
+                                    <input type="password" class="form-control" id="password" name="password" placeholder="Senha" required>
+                                </div>
+                                    <button type="submit" class="btn btn-primary btn-block">ENTRAR</button>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <div class="text-center">
+                                <p> Caso não saiba seus dados, fale com um administrador </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </nav>
--- www/index.php
+++ www/index.php
@@ -114,73 +114,15 @@
             js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.1';
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
-
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php"> <img src="https://static.tumblr.com/jjqbi6t/YWnpe5fxy/logo.png"
-                        class="aex-logo img-circle"> </a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li> <a href="#sobre"> SOBRE </a></li>
-                    <!--<li> <a href="https://autoexpresslogistica.tumblr.com/colaboradores"> COLABORADORES </a></li> -->
-                    <li> <a href="#contato"> CONTATO </a></li>
-                    <li> <a href="recrutamento.html"> RECRUTAMENTO </a></li>
-                    <li> <?php 
-                            if (!isset($_SESSION['usuarioID'])) { 
-                                echo "<button type='button' class='btn btn-info btn-login'  data-toggle='modal'
-                                                        data-target='#modalLogin'>LOGIN</button>'}";
-                                
-                            } 
-                                else { 
-                                    echo "<a href='colaborador.php'> COLABORADOR </a> ";
-                                   }
-                        ?>                
-                    <li>
-
-                </ul>
-            </div>
-            <div class="modal fade " tabindex="-1" role="dialog" id="modalLogin">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header header-promocao">
-                            <h5 class="modal-title-center">LOGIN</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="login.php" id="loginForm" method="POST" autocomplete="on">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">USUÁRIO</label>
-                                    <input type="text" class="form-control" id="user" name="user" aria-describedby="emailHelp" placeholder="Usuário" required>
-                                    
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">SENHA</label>
-                                    <input type="password" class="form-control" id="password" name="password" placeholder="Senha" required>
-                                </div>
-                                    <button type="submit" class="btn btn-primary btn-block">ENTRAR</button>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <div class="text-center">
-                                <p> Caso não saiba seus dados, fale com um administrador </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-    </nav>
+    <script> 
+        $(function (){
+            $("#header").load("header.php");
+            $("#footer").load("footer.html");
+        });
+    </script>
+    <div id="header"> </div>
+    
+    
     <script type="text/javascript">
         jQuery(function () {
             $(".navbar-toggle").click(function () {
@@ -453,30 +395,7 @@
     </div>
 
     <hr>
-    <footer id="rodape" class="container-fluid text-center">
-        <div class="row">
-            <div class="col-12">
-                <img class="img-circle" src="https://static.tumblr.com/jjqbi6t/YWnpe5fxy/logo.png" height="250px">
-            </div>
-            <div class="col-12 creditos">
-
-                Auto Express Logistica <br> 2020 <br>
-
-            </div>
-        </div>
-            
-        
-        <div class="row git">
-            <div class="col-12">
-            <a href="https://github.com/ariel7198"> <img src="https://i.imgur.com/SL1dLS5.png" height="30px"> ariel7198</a>
-            </div>
-        </div>
-
-            
-
-
-        
-    </footer>
+    <div id="footer"></div>
 
 </body>
 
